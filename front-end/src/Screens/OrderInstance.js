79434fd677828db2941e9569d38c4ede69e9d01f
DESKTOP-9NGS0J6\hao15 <hao152903@gmail.com>
2021-10-27 14:01:32 +0700

    feat[reactjs]: create instance order view, and order flow.

--- front-end/src/Screens/OrderInstance.js
+++ front-end/src/Screens/OrderInstance.js
@@ -8,7 +8,8 @@ import storeIcon from '../assets/store.png';
 import motorIcon from '../assets/motor.png';
 import 'leaflet/dist/leaflet.css';
 import {
-    blueColor
+    blueColor,
+    orangeColor
 } from '../contants';
 import LoadingGif from '../assets/loading.gif';
 
@@ -19,27 +20,201 @@ class OrderInstance extends Component {
         this.state = {
             instanceOrder: {
                 id: '1',
-                status: '1',
-                name: 'Mì SO lòng đào siêu ngon',
-                description: 'Mì cực ngon',
-                price: 40000
-            }
+                status: 2,
+            },
+            listFoodsInstance: [
+                {
+                    'id': 1,
+                    'name': 'Mì xào giòn',
+                    'description': 'Ngon say lalalala',
+                    'categories:': 'Mì, thịt bò, bông cải xào',
+                    'price': 20000
+                },
+                {
+                    'id': 2,
+                    'name': 'Mì xào giòn',
+                    'description': 'Ngon say lalalala',
+                    'categories:': 'Mì, thịt bò, bông cải xào',
+                    'price': 40000
+                }
+            ]
         }
+        this.handleGetOrder = this.handleGetOrder.bind(this);
+    }
+
+    handleGetOrder = () => {
+        let tmpContext = {
+            id: '1',
+            status: 1,
+        }
+        this.setState({
+            instanceOrder: tmpContext
+        });
     }
 
     mainView = () => {
         if(this.state.instanceOrder != null) {
-            return(
-                <div style = {{
-                    height: '520px',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
-
-                </div>
-            );
+            if(this.state.instanceOrder.status === 1) {
+                return(
+                    <div style = {{
+                        height: '520px',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                        <div style = {{
+                            border: 'solid 0.2px #e6e6e6',
+                            boxShadow: '5px 10px 18px #888888',
+                            height: '90%',
+                            width: '70%'
+                        }}>
+                            <MapContainer style = {{
+                                height: '100%',
+                                width: '100%',
+                                border: 'solid 0.5px grey'
+                            }} center={[14.058324, 108.277199]} zoom={8} scrollWheelZoom={true}>
+                                <TileLayer
+                                    attribution='Vị trí đơn hàng'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={[21.0245, 105.84117]}>
+                                    <Popup>
+                                        Vị trí giao dự kiến
+                                    </Popup>
+                                </Marker>
+                            </MapContainer>
+                        </div>
+                        <div style = {{
+                            width: '360px',
+                            border: 'solid 0.5px grey',
+                            marginLeft: '20px',
+                            borderRadius: '10px',
+                            padding: '20px',
+                            display: 'flex',
+                            flexDirection: 'column'
+                        }}>
+                            <div style = {{
+                                display: 'flex',
+                                flexDirection: 'row'
+                            }}>
+                                <img src = {storeIcon} style = {{
+                                    height: '30px',
+                                    width: '30px'
+                                }}></img>
+                                <p style = {{
+                                    marginLeft: '5px',
+                                    fontWeight: 'bold',
+                                    color: blueColor
+                                }}>Mì SO - Lòng đào</p>
+                            </div>
+                            <p>Số 14 đường Quản Trọng Hoàng, Hưng Lợi, Ninh Kiều</p>
+                            <div style = {{
+                                height: '0.5px',
+                                backgroundColor: 'grey'
+                            }}></div>
+                            <div style = {{
+                                display: 'flex',
+                                flexDirection: 'row'
+                            }}>
+                                <p>Trạng thái: </p><p style = {{
+                                    color: 'green',
+                                    fontWeight: 'bold',
+                                    marginLeft: '10px'
+                                }}>Đang giao</p>
+                            </div>
+                            <div style = {{
+                                display: 'flex',
+                                flexDirection: 'row'
+                            }}>
+                                <p>Tổng tiền: </p><p style = {{
+                                    fontWeight: 'bold',
+                                    marginLeft: '10px'
+                                }}>70.000 vnđ</p>
+                            </div>
+                            <div style = {{
+                                display: 'flex',
+                                flexDirection: 'row'
+                            }}>
+                                <p>Mã đơn hàng: </p><p style = {{
+                                    fontWeight: 'bold',
+                                    marginLeft: '10px'
+                                }}>DSAJIH321</p>
+                            </div>
+                        </div>
+                    </div>
+                );
+            } else if(this.state.instanceOrder.status === 2) {
+                const item = this.state.listFoodsInstance.map((item, index) => {
+                    return(
+                        <div style = {{
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}>
+                            <div style = {{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'space-between'
+                            }}>
+                                <p style = {{fontWeight: 'bold'}}>{item.name}</p> <p>{item.price} vnđ</p>
+                            </div>
+                            <div style = {{
+                                height: '0.5px',
+                                border: 'solid 0.5px grey'
+                            }}></div>
+                        </div>
+                    );
+                })
+                return(
+                    <div style = {{
+                        height: '520px',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}> 
+                        <div style = {{
+                            border: 'solid 0.2px #e6e6e6',
+                            boxShadow: '5px 10px 18px #888888',
+                            height: '90%',
+                            width: '40%',
+                            padding: '20px'
+                        }}>
+                            <div style = {{
+                                display: 'flex',
+                                flexDirection: 'column' 
+                            }}>
+                                <p style = {{
+                                    fontWeight: 'bold'
+                                }}>Chi tiết đơn hàng</p>
+                                {item}
+                                <div style = {{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between' ,
+                                    marginTop: '20px'
+                                }}>
+                                    <p style = {{
+                                        fontWeight: 'bold'
+                                    }}>Tổng cộng: </p>
+                                    <p>100000 vnđ</p>
+                                </div>
+                                <button onClick = {() => {
+                                    this.handleGetOrder()
+                                }} style = {{
+                                    fontWeight: 'bold',
+                                    color: 'white',
+                                    backgroundColor: orangeColor,
+                                    border: 'none',
+                                    height: '36px',
+                                    borderRadius: '10px'
+                                }}>Đặt hàng</button>
+                            </div>
+                        </div>
+                    </div>
+                );
+                
+            }
         } else {
             return(
                 <div style = {{
@@ -56,97 +231,6 @@ class OrderInstance extends Component {
                 </div>
             );
         }
-
-        // View if order was delivered
-        // return(
-        //     <div style = {{
-        //         height: '520px',
-        //         display: 'flex',
-        //         flexDirection: 'row',
-        //         justifyContent: 'center',
-        //         alignItems: 'center'
-        //     }}>
-        //         <div style = {{
-        //             border: 'solid 0.2px #e6e6e6',
-        //             boxShadow: '5px 10px 18px #888888',
-        //             height: '90%',
-        //             width: '70%'
-        //         }}>
-        //             <MapContainer style = {{
-        //                 height: '100%',
-        //                 width: '100%',
-        //                 border: 'solid 0.5px grey'
-        //             }} center={[14.058324, 108.277199]} zoom={8} scrollWheelZoom={true}>
-        //                 <TileLayer
-        //                     attribution='Vị trí đơn hàng'
-        //                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        //                 />
-        //                 <Marker position={[21.0245, 105.84117]}>
-        //                     <Popup>
-        //                         Vị trí giao dự kiến
-        //                     </Popup>
-        //                 </Marker>
-        //             </MapContainer>
-        //         </div>
-        //         <div style = {{
-        //             width: '360px',
-        //             border: 'solid 0.5px grey',
-        //             marginLeft: '20px',
-        //             borderRadius: '10px',
-        //             padding: '20px',
-        //             display: 'flex',
-        //             flexDirection: 'column'
-        //         }}>
-        //             <div style = {{
-        //                 display: 'flex',
-        //                 flexDirection: 'row'
-        //             }}>
-        //                 <img src = {storeIcon} style = {{
-        //                     height: '30px',
-        //                     width: '30px'
-        //                 }}></img>
-        //                 <p style = {{
-        //                     marginLeft: '5px',
-        //                     fontWeight: 'bold',
-        //                     color: blueColor
-        //                 }}>Mì SO - Lòng đào</p>
-        //             </div>
-        //             <p>Số 14 đường Quản Trọng Hoàng, Hưng Lợi, Ninh Kiều</p>
-        //             <div style = {{
-        //                 height: '0.5px',
-        //                 backgroundColor: 'grey'
-        //             }}></div>
-        //             <div style = {{
-        //                 display: 'flex',
-        //                 flexDirection: 'row'
-        //             }}>
-        //                 <p>Trạng thái: </p><p style = {{
-        //                     color: 'green',
-        //                     fontWeight: 'bold',
-        //                     marginLeft: '10px'
-        //                 }}>Đang giao</p>
-        //             </div>
-        //             <div style = {{
-        //                 display: 'flex',
-        //                 flexDirection: 'row'
-        //             }}>
-        //                 <p>Tổng tiền: </p><p style = {{
-        //                     fontWeight: 'bold',
-        //                     marginLeft: '10px'
-        //                 }}>70.000 vnđ</p>
-        //             </div>
-        //             <div style = {{
-        //                 display: 'flex',
-        //                 flexDirection: 'row'
-        //             }}>
-        //                 <p>Mã đơn hàng: </p><p style = {{
-        //                     fontWeight: 'bold',
-        //                     marginLeft: '10px'
-        //                 }}>DSAJIH321</p>
-        //             </div>
-        //         </div>
-        //     </div>
-        // );
     }
 
     render() {
