4cfd49d5ad34f89c301899d9dd536466d8b5ac1f
HaoHuynh0301 <hao152903@gmail.com>
2021-11-16 17:34:20 +0700

    feat[django]: add static link into urlpattern to get product's image.

--- shopee_food_clone/food_order/urls.py
+++ shopee_food_clone/food_order/urls.py
@@ -1,21 +1,8 @@
-"""shopee_food_clone URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -46,4 +33,4 @@ urlpatterns = [
     path('api/shipping-address/', views.InstanceAddressView.as_view(), name = 'shipping-address'),
     path('api/delivered-orders/', views.DeliveredOrderView.as_view(), name = 'delivered-orders'),
     path('api/re-order/', views.ReOrderView.as_view(), name = 're-order')
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
