cdda25dd8303a47005d67826128f2d99fdf101f0
HaoHuynh0301 <hao152903@gmail.com>
2021-11-24 20:40:45 +0700

    style[reactjs]: Update the footer's interface.

--- front-end/src/Components/Footer.js
+++ front-end/src/Components/Footer.js
@@ -1,57 +1,56 @@
-import shopeefoodLogo from '../assets/shopeefoodlogo.jpg';
 import appstore from '../assets/appstore.png';
-import chplay from '../assets/chplay.jpg';
+import './footer.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    withRouter 
+} from "react-router-dom";
 
 function Footer() {
     return(
-        <div style = {{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            borderTop: 'solid 0.2px #cccccc',
-            paddingTop: '20px'
+        <footer className="w-100 py-4 flex-shrink-0" style = {{
+            borderTop: 'solid 0.5px grey'
         }}>
-            <div>
-                <p style = {{fontSize: '16px', marginBottom: '10px', fontWeight: 'bold'}}>Công ty</p>
-                <p style = {{color: 'blue'}}>Giới thiệu</p>
-                <p style = {{color: 'blue'}}>Trung tâm trợ giúp</p>
-                <p style = {{color: 'blue'}}>Điều khoản sử dụng</p>
+            <div className="container py-4">
+            <div className="row gy-4 gx-5">
+                <div className="col-lg-4 col-md-6">
+                <h5 className="h1 text-dark">CONTACT</h5>
+                <p className="small text-muted">Bạn có thể trao đổi với chúng tôi qua email</p>
+                <p className="small text-muted mb-0"><a className="text-primary" href="#">thuanhaoctu@gmail.com</a></p>
+                </div>
+                <div className="col-lg-2 col-md-6">
+                <h5 className="text-dark mb-3">Quick links</h5>
+                <ul className="list-unstyled text-muted">
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Thông tin</a></li>
+                    <li><a href="#">Đơn hàng</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+                </div>
+                <div className="col-lg-2 col-md-6">
+                <h5 className="text-dark mb-3">Tải ứng dụng tại</h5>
+                    <img src = {appstore} style = {{
+                        height: '100px',
+                        width: '170px',
+                        
+                    }}></img>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                <h5 className="text-dark mb-3">Hợp tác</h5>
+                <p className="small text-muted">Hãy để lại số điện thoại của bạn, chúng tôi sẽ sớm liên hệ.</p>
+                <form action="#">
+                    <div className="input-group mb-3">
+                    <input className="form-control" type="text" placeholder="Số điện thoại" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button className="btn btn-primary" id="button-addon2" type="button"><i className="fas fa-paper-plane" /></button>
+                    </div>
+                </form>
+                </div>
             </div>
-            <div style = {{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
-            }}>
-                <p style = {{fontSize: '16px', marginBottom: '10px', fontWeight: 'bold'}}>Ứng dụng NotShoppeFood</p>
-                <img style = {{
-                    height: '70px',
-                    width: '200px',
-                    borderRadius: '10px'
-                }} src = {appstore}></img>
-                <img style = {{
-                    height: '70px',
-                    width: '200px',
-                    borderRadius: '10px'
-                }} src = {chplay}></img>
             </div>
-            <div style = {{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
-            }}>
-                <img style = {{
-                    height: '120px',
-                    width: '120px',
-                    borderRadius: '10px'
-                }} src = {shopeefoodLogo}></img>
-                <p style = {{color: 'grey', fontSize: '13px', marginTop: '15px'}}>2021@ NotShoppeFood</p>
-            </div>
-            <div>
-                <p style = {{fontSize: '16px', marginBottom: '10px', fontWeight: 'bold'}}>Địa chỉ công ty</p>
-                <p style = {{color: 'blue'}}>Số 14, Quản Trọng Hoàng, Ninh Kiều</p>
-                <p style = {{color: 'blue'}}>Khu 2, đại học Cần Thơ</p>
-            </div>
-        </div>
+        </footer>
     );
 }
 
